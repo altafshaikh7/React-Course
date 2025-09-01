465a3d832c64cc281812078741f70b1d7741bcc0
altafshaikh7 <altafshaikh07781@gmail.com>
2025-09-02 04:17:55 +0530

    lec 9

--- frontend/src/components/TextForm.js
+++ frontend/src/components/TextForm.js
@@ -16,6 +16,24 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleClearClick = () => {
+    console.log("Clear was clicked");
+    setText("");
+  };
+
+  const handleCopyClick = () => {
+    console.log("Copy was clicked");
+    navigator.clipboard.writeText(text);
+  };
+
+  const handleaLtErNaTecAsEClick = () => {
+    console.log("Alternating case was clicked");
+    let newText = text.split("").map((char, index) => {
+      return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+    }).join("");
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
@@ -42,6 +60,16 @@ export default function TextForm(props) {
       <button className="btn btn-primary -lowercase mx-2 my-2" onClick={handleLoClick}>
         Convert To Lowercase
       </button>
+      <button className="btn btn-primary -clear mx-2 my-2" onClick={() => setText("")}>
+        Clear Text
+      </button>
+      <button className="btn btn-primary -copy mx-2 my-2" onClick={() => navigator.clipboard.writeText(text)}>
+        Copy Text
+      </button>
+      <button className="btn btn-primary -inverse mx-2 my-2" onClick={handleaLtErNaTecAsEClick}>
+       aLtErNaTe cAsE
+      </button>
+
     </div>
     <div className="container my-2">
         <h1>Your Text Summary</h1>
