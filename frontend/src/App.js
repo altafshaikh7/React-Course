fa79de7c9c67928f156b01c9a440e3962e470e98
altafshaikh7 <altafshaikh07781@gmail.com>
2025-09-02 03:10:59 +0530

    lec 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
 
       <>
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
-
+      <div className="container my-3">
+        <TextForm heading="Enter Text to Analyze Below"/>
+      </div>
+      
       </>
   );
 }
