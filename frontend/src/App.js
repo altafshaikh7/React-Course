8508379d815bf6e24732a33570a1822941d1ade3
altafshaikh7 <altafshaikh07781@gmail.com>
2025-09-03 21:33:53 +0530

    update about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+import About from './components/About';
+// import TextForm from './components/TextForm'; 
+
+
+
+
 function App() {
   return (
 
       <>
       <Navbar title="TextUtils" aboutText="About TextUtils"/>
       <div className="container my-3">
-        <TextForm heading="Enter Text to Analyze Below"/>
+        {/* <TextForm heading="Enter Text to Analyze Below"/> */}
+     <About/>
       </div>
       
       </>
