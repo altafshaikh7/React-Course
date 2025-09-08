8288332b5018d1e743c5e423dadc2d304c3610b9
altafshaikh7 <altafshaikh07781@gmail.com>
2025-09-09 01:55:25 +0530

    lec 26

--- newsapp/src/Components/News.jsx
+++ newsapp/src/Components/News.jsx
@@ -241,7 +241,7 @@ export class News extends Component {
         <div className="row">
           {this.state.articles.map((element)=>{
            return <div className="col-md-4" key={element.url}>
-            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+            <NewsItem title={element.title.slice(0,35 )} description={element.description.slice(0, 70)} imageUrl={element.urlToImage} newsUrl={element.url} />
           </div>;
         })}
       </div>
